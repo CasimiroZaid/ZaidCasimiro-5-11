44df98d571f360bcee846857c579a047750bbbf5
Zaid Casimiro <zaidcasimiro.cz@gmail.com>
2026-04-03 01:36:48 -0300

    Subo la actividad 6

---
+++ source/repos/ZaidCasimiro-Act-6/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1. Desarrollar una clase que represente un punto en el plano y tenga los
+siguientes métodos: cargar los valores de x e y, imprimir en que cuadrante se
+encuentra dicho punto (concepto matemático, primer cuadrante si x e y son
+positivas, si x&lt;0 e y&gt;0 segundo cuadrante, etc.)
+*/
+
+namespace Punto1
+{
+    class Punto
+    {
+        private int x;
+        private int y;
+
+        public void cargarDatos()
+        {
+            Console.Write("coordenada X: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("coordenada Y: ");
+            y = int.Parse(Console.ReadLine());
+        }
+
+        public void cuadrante()
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Primer Cuadrante");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Segundo Cuadrant");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Tercer Cuadrante");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Cuarto Cuadrante");
+            }
+            else
+            {
+                Console.WriteLine("Esta en cero");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto n = new Punto();
+
+            n.cargarDatos();
+            n.cuadrante();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ source/repos/ZaidCasimiro-Act-6/Punto3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*3. Confeccionar una clase que represente un empleado. Definir como atributos su
+nombre y su sueldo. Confeccionar los métodos para la carga, otro para imprimir
+sus datos y por último uno que imprima un mensaje si debe pagar impuestos
+(si el sueldo supera a 3000).
+*/
+
+namespace Punto3
+{
+    class Empleado
+    {
+        private string nombre;
+        private float sueldo;
+
+        public void CargarDatos()
+        {
+            Console.Write("Ingrese el nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese el sueldo: ");
+            sueldo = float.Parse(Console.ReadLine());
+        }
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Datos del empleado");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Sueldo: " + sueldo);
+        }
+
+        public void Impuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("El empleado tiene que pagar impuestos");
+            }
+            else
+            {
+                Console.WriteLine("El empleado no paga impuestos");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado1 = new Empleado();
+
+            empleado1.CargarDatos();
+            empleado1.ImprimirDatos();
+            empleado1.Impuestos();
+
+            Console.ReadKey();
+        }
+    }
+}
