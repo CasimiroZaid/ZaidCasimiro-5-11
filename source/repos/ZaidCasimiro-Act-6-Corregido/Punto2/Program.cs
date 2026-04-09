f068b767381c22bc7cf3eaeb33abfd51b82f2a67
Zaid Casimiro <zaidcasimiro.cz@gmail.com>
2026-04-09 09:13:45 -0300

    Subo la actividad 6 con las correcciones indicadas

---
+++ source/repos/ZaidCasimiro-Act-6-Corregido/Punto2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 2. Desarrollar una clase que represente un Cuadrado y tenga los siguientes
+métodos: cargar el valor de su lado, imprimir su perímetro y su superficie.
+*/
+
+namespace Punto2
+{
+    class Cuadrado
+    {
+        private int lado;
+
+        public void LadoCuadrado()
+        {
+            Console.Write("Ingrese lado del cuadrado: ");
+            lado = int.Parse(Console.ReadLine());
+        }
+
+        public void ImprimirPerimetro()
+        {
+            int perimetro = lado * 4;
+            Console.WriteLine("El perímetro es: " + perimetro);
+        }
+
+        public void ImprimirSuperficie()
+        {
+            int superficie = lado * lado;
+            Console.WriteLine("La superficie es: " + superficie);
+        }
+
+        static void Main(string[] args)
+        {
+            Cuadrado n = new Cuadrado();
+
+            n.LadoCuadrado();
+            n.ImprimirPerimetro();
+            n.ImprimirSuperficie();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ source/repos/ZaidCasimiro-Act-6-Corregido/Punto4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 4. Implementar la clase operaciones. Se deben cargar dos valores enteros,
+calcular su suma, resta, multiplicación y división, cada una en un método,
+imprimir dichos resultados.
+*/
+
+namespace Punto4
+{
+    class Operaciones
+    {
+        private int valor1;
+        private int valor2;
+
+        public void CargarDatos()
+        {
+            Console.Write("Ingrese el primer valor entero: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo valor entero: ");
+            valor2 = int.Parse(Console.ReadLine());
+        }
+
+        public void Sumar()
+        {
+            int suma = valor1 + valor2;
+            Console.WriteLine("La suma es: " + suma);
+        }
+
+        public void Restar()
+        {
+            int resta = valor1 - valor2;
+            Console.WriteLine("La resta es: " + resta);
+        }
+
+        public void Multiplicar()
+        {
+            int multiplicacion = valor1 * valor2;
+            Console.WriteLine("La multiplicación es: " + multiplicacion);
+        }
+
+        public void Dividir()
+        {
+                int division = valor1 / valor2;
+                Console.WriteLine("La división es: " + division);
+        }
+
+        static void Main(string[] args)
+        {
+            Operaciones calcular = new Operaciones();
+
+            calcular.CargarDatos();
+            calcular.Sumar();
+            calcular.Restar();
+            calcular.Multiplicar();
+            calcular.Dividir();
+
+            Console.ReadKey();
+        }
+    }
+}
